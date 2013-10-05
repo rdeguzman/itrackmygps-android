2b379e7e98fec5a7d2a5ec3e8e134dd5bdf7732d
Rupert <rupert@2rmobile.com>
2013-10-05 10:43:26 +1000

    Add "settings" ActionButton in ActionBar

--- src/com/twormobile/gpslogger/MainActivity.java
+++ src/com/twormobile/gpslogger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.twormobile.gpslogger;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -7,6 +8,9 @@ import android.content.IntentFilter;
 import android.location.*;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -68,6 +72,9 @@ public class MainActivity extends Activity {
         tvGPSAccuracy = (TextView)findViewById(R.id.tvGPSAccuracy);
         tvGPSProvider = (TextView)findViewById(R.id.tvGPSProvider);
         tvGPSTotalSatellites = (TextView)findViewById(R.id.tvGPSFixTotalSatellites);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.show();
 
         gpsSatelliteList = new ArrayList<GpsSatellite>();
 
@@ -165,14 +172,39 @@ public class MainActivity extends Activity {
 
     public void onPause() {
         super.onPause(); // Always call the superclass method first
-        Log.i(TAG, "paused");
+        Log.d(TAG, "paused");
     }
 
     public void onResume() {
         super.onResume();  // Always call the superclass method first
 
-        Log.i(TAG, "resume");
+        Log.d(TAG, "resume");
         updateButtons();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startSettingsActivity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void startSettingsActivity(){
+        Log.d(TAG, "startSettingsActivity");
     }
 
 }

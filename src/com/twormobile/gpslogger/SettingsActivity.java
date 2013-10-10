bcd47f0cb2048d3415ee28c8b64b6e3d1073bd04
Rupert <rupert@datalinktech.com.au>
2013-10-10 17:15:50 +1100

    Rename spinner to spinner_map_layers

--- src/com/twormobile/gpslogger/SettingsActivity.java
+++ src/com/twormobile/gpslogger/SettingsActivity.java
@@ -7,10 +7,8 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 import com.google.android.gms.maps.GoogleMap;
 
 public class SettingsActivity extends Activity {
@@ -25,7 +23,7 @@ public class SettingsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        mapLayerSpinner = (Spinner)findViewById(R.id.spinner1);
+        mapLayerSpinner = (Spinner)findViewById(R.id.spinner_map_layers);
         populate();
 
         Context context = getApplicationContext();

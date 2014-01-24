4bf8d4e1a1b0d3896165a1868529e7ecdeca2104
rupert <rupert@2rmobile.com>
2014-01-24 12:37:40 +1100

    Add LoginActivity

---
+++ src/com/twormobile/trackble/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.twormobile.trackble;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

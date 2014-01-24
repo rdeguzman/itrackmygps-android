902e5314738daaba9c775152cea6e1ff4e474994
rupert <rupert@2rmobile.com>
2014-01-24 12:07:26 +1100

    Adds initial RegisterActivity

---
+++ src/com/twormobile/trackble/RegisterActivity.java
@@ -0,0 +1,11 @@
+package com.twormobile.trackble;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}

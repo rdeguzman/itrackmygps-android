dc54f356bd43b2087c4c66c10db0ce7ea39ea323
rupert <rupert@2rmobile.com>
2014-01-25 10:03:55 +1100

    Add buttonCancelPressed and buttonSubmitPressed

--- src/com/twormobile/trackble/LoginActivity.java
+++ src/com/twormobile/trackble/LoginActivity.java
@@ -1,11 +1,19 @@
 package com.twormobile.trackble;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void buttonCancelPressed(View view){
+        Intent resultIntent = new Intent();
+        setResult(Activity.RESULT_CANCELED, resultIntent);
+        super.finish();
     }
 }
--- src/com/twormobile/trackble/RegisterActivity.java
+++ src/com/twormobile/trackble/RegisterActivity.java
@@ -1,11 +1,24 @@
 package com.twormobile.trackble;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+    public void buttonCancelPressed(View view){
+        Intent resultIntent = new Intent();
+        setResult(Activity.RESULT_CANCELED, resultIntent);
+        super.finish();
+    }
+
+    public void buttonSubmitPressed(View view){
+
+    }
+
 }

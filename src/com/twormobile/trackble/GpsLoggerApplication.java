6306bbfcc8301dbc7ebc5991aa3dd8a470f8bf51
rupert <rupert@2rmobile.com>
2014-01-25 09:02:20 +1100

    Add isLoggedIn(), getUsername(), checkUser() in GpsLoggerApplication

--- src/com/twormobile/trackble/GpsLoggerApplication.java
+++ src/com/twormobile/trackble/GpsLoggerApplication.java
@@ -1,6 +1,8 @@
 package com.twormobile.trackble;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -17,9 +19,17 @@ public class GpsLoggerApplication extends Application {
     private boolean bServiceRun;
     private RequestQueue queue;
 
+    private SharedPreferences prefs;
+
+    private boolean bLoggedIn = false;
+    private String username;
+
     @Override
     public void onCreate() {
         setURLs();
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        checkUser();
 
         Log.i(TAG, "onCreated");
         uuidFactory = new DeviceUUIDFactory(getApplicationContext());
@@ -57,5 +67,21 @@ public class GpsLoggerApplication extends Application {
 
     public void setURLs(){
         LOCATION_NEW_URL = getResources().getString(R.string.NEW_LOCATION_URL);
+    }
+
+    public boolean isLoggedIn() {
+        return bLoggedIn;
+    }
+
+    public String getUsername() { return username; }
+
+    private void checkUser() {
+        username = prefs.getString("username", null);
+        if(username == null) {
+            bLoggedIn = false;
+        }
+        else {
+            bLoggedIn = true;
+        }
     }
 }

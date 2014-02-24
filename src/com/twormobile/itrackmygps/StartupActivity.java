1d3704ea5d0788f657ad23791c8fc512c0d7ef3b
rupert <rupert@2rmobile.com>
2014-02-24 16:45:17 +1100

    Add StartupActivity

---
+++ src/com/twormobile/itrackmygps/StartupActivity.java
@@ -0,0 +1,13 @@
+package com.twormobile.itrackmygps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartupActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+    }
+}

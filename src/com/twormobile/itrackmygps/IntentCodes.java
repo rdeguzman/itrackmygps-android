2fd9c2b8be40212f21de2c4b4c8a44784cc8b881
rupert <rupert@2rmobile.com>
2014-02-25 17:20:25 +1100

    Change traces of trackble to itrackmygps

--- src/com/twormobile/itrackmygps/IntentCodes.java
+++ src/com/twormobile/itrackmygps/IntentCodes.java
@@ -5,6 +5,6 @@ public class IntentCodes {
     public static final int LOGIN = 20;
     public static final int SETTINGS = 30;
 
-    public static final String ACTION_LOCATION              = "com.twormobile.trackble.ACTION_LOCATION";
-    public static final String ACTION_GPS_NETWORK_STATUS    = "com.twormobile.trackble.ACTION_GPS_NETWORK_STATUS";
+    public static final String ACTION_LOCATION              = "com.twormobile.itrackmygps.ACTION_LOCATION";
+    public static final String ACTION_GPS_NETWORK_STATUS    = "com.twormobile.itrackmygps.ACTION_GPS_NETWORK_STATUS";
 }

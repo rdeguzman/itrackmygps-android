6fecadfc5e8be3b342ed7dee99077faf78236ce7
rupert <rupert@2rmobile.com>
2014-05-22 09:41:26 +1000

    Fix #21 bug when sending SMS on Android 4.4 for KitKat

--- src/com/twormobile/itrackmygps/ShareActivity.java
+++ src/com/twormobile/itrackmygps/ShareActivity.java
@@ -3,8 +3,10 @@ package com.twormobile.itrackmygps;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.Telephony;
 import com.twormobile.itrackmygps.android.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -42,10 +44,30 @@ public class ShareActivity extends Activity {
         if(isMessageValid()) {
             String message = String.valueOf(etxtMessage.getText()).trim();
 
-            Intent sendIntent = new Intent(Intent.ACTION_VIEW);
-            sendIntent.putExtra("sms_body", message);
-            sendIntent.setType("vnd.android-dir/mms-sms");
-            startActivity(sendIntent);
+            //At least KitKat
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                //Need to change the build to API 19
+                String defaultSmsPackageName = Telephony.Sms.getDefaultSmsPackage(this);
+
+                Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                sendIntent.setType("text/plain");
+                sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+
+                // Can be null in case that there is no default, then the user would be able
+                // to choose any app that support this intent.
+                if (defaultSmsPackageName != null) {
+                    sendIntent.setPackage(defaultSmsPackageName);
+                }
+
+                startActivity(sendIntent);
+
+            }
+            else  {
+                // For early versions we just use ACTION_VIEW
+                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                sendIntent.putExtra("sms_body", message);
+                startActivity(sendIntent);
+            }
         }
     }
 
